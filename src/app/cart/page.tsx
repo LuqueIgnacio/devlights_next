1a53e6d48dccb1365d879b85365c70f81760d3d3
LuqueIgnacio <ignacioluque.micha@gmail.com>
2024-10-03 23:19:09 -0300

    Fix carrito

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,6 +5,14 @@ import CartProduct from '@/components/CartProduct'
 
 export default function page() {
   const {products} = useCartContext()
+
+  if(products.length < 1){
+    return (
+      <div className='flex flex-col lg:m-auto lg:flex-row items-center border p-4 drop-shadow-xl'>
+          <p>El carrito está vacio</p>
+      </div>
+  )
+  }
   return (
     <div className='flex flex-col lg:m-auto lg:w-3/4 gap-4'>
       {products.map(product => {
--- src/components/CartProduct/index.tsx
+++ src/components/CartProduct/index.tsx
@@ -7,48 +7,37 @@ import { useCartContext } from '@/providers/CartContextProvider'
 export default function CartProduct({product}: {product: ICartProduct}) {
     const {products, addProduct, minusProduct, deleteProduct} = useCartContext()
 
-    if(products.length > 0 ){
-        return (
-            <div className='flex flex-col lg:flex-row items-center border p-4 drop-shadow-xl'>
-                <div className='flex w-20 h-20 lg:w-32 lg:h-32'>
-                    <Image 
-                    src={product.image} 
-                    width={300}
-                    height={300}
-                    alt={product.title}
-                    className='w-full h-full'
-                    />
-                </div>
-    
-                <div className='w-1/3'>
-                <p className='font-semibold truncate text-center'>{product.title}</p>
-                </div>
-    
-                <div className='w-1/12'>
-                    <p className='text-center font-semibold text-lg'>${product.price}</p>
-                </div>
-    
-                <div className='flex justify-center items-center gap-3 w-2/3 '>
-                    {product.quantity > 1 ? (
-                        <button onClick={() => minusProduct(product)} className='p-2 text-lg text-slate-50 bg-slate-500 hover:bg-slate-700 rounded-full'>-</button>
-                    ): null}
-                    <p>Cantidad: {product.quantity}</p>
-                    <button onClick={() => addProduct(product)} className='p-2 text-lg text-slate-50 bg-slate-500 hover:bg-slate-700 rounded-full'>+</button>
-                </div>
-    
-                <div className=''>
-                    <button onClick={() => deleteProduct(product)} className='p-2 text-lg text-slate-50 bg-slate-500 hover:bg-slate-700 rounded-md'>Borrar</button>
-                </div>
+    return (
+        <div className='flex flex-col lg:flex-row items-center border p-4 drop-shadow-xl'>
+            <div className='flex w-20 h-20 lg:w-32 lg:h-32'>
+                <Image 
+                src={product.image} 
+                width={300}
+                height={300}
+                alt={product.title}
+                className='w-full h-full'
+                />
             </div>
-        )
-    }else{
-        return (
-            <div >
-                <p>El carrito está vacio</p>
-            </div>
-        )
-    }
 
-    
-    
+            <div className='w-1/3'>
+            <p className='font-semibold truncate text-center'>{product.title}</p>
+            </div>
+
+            <div className='w-1/12'>
+                <p className='text-center font-semibold text-lg'>${product.price}</p>
+            </div>
+
+            <div className='flex justify-center items-center gap-3 w-2/3 '>
+                {product.quantity > 1 ? (
+                    <button onClick={() => minusProduct(product)} className='p-2 text-lg text-slate-50 bg-slate-500 hover:bg-slate-700 rounded-full'>-</button>
+                ): null}
+                <p>Cantidad: {product.quantity}</p>
+                <button onClick={() => addProduct(product)} className='p-2 text-lg text-slate-50 bg-slate-500 hover:bg-slate-700 rounded-full'>+</button>
+            </div>
+
+            <div className=''>
+                <button onClick={() => deleteProduct(product)} className='p-2 text-lg text-slate-50 bg-slate-500 hover:bg-slate-700 rounded-md'>Borrar</button>
+            </div>
+        </div>
+    )
 }
